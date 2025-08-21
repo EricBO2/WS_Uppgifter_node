1287176299d6608ee28724ea5ec18b4557bb568e
EricBO2 <eric.carlsson@stud.sti.se>
2025-08-21 08:52:19 +0200

    ändra response till json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 const port: number = 3000;
 
 app.get("/", (request, response) => {
   response.send("Hello world!");
+  response.json()
 });
+
+app.listen("/", ()=>{
+    console.log("Hello vietnam!!!!")
+})
