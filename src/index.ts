7812d90ebb286cf6e3a1ab2c07bc5a2ea9c6608e
EricBO2 <eric.carlsson@stud.sti.se>
2025-08-22 12:11:07 +0200

    add massa saker för lection 2

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
-import express, { response } from "express";
+import express from "express"
+//import "dotenv/config" // oneliner for configuration
+import { closeDB, runDB } from "./db/database.js"
+import { env } from "node:process";
+
+async function startServer() {
+  try {
+    await runDB();
+    app.listen(port, () => {
+      console.log(`Listening to port ${port}`);
+      console.log(`Start the app : http://localhost:${port}`);
+    });
+    process.on("SIGINT", async () => {
+      console.log("Cleaning up ..");
+      await closeDB();
+      process.exit(0);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+startServer();
 
 const app = express();
-const port: number = 3000;
+const port: number = Number(env.port)|| 3000;
+const address: string = "0.0.0.0"
 
 app.get("/", (request, response) => {
-  response.send("Hello world!");
-  response.json()
+  response.status(200).send({ message: "Hello world!" });
 });
 
-app.listen("/", ()=>{
-    console.log("Hello vietnam!!!!")
+app.listen("/", () => {
+  console.log("Hello vietnam!!!!");
+});
+
+app.listen(port,address, () => {
+  console.log(`Listeing to port ${port}`)
 })
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send({ id: id });
+});
